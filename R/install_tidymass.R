be3705a16781701a528e3f2386e6ed64074acb26
jaspershen <shenxt1990@outlook.com>
2022-09-20 16:42:28 -0700

    update

--- R/install_tidymass.R
+++ R/install_tidymass.R
@@ -1,6 +1,71 @@
-install_tidymass <- function() {
-  ##download masstools first
-  
-  utils::download.file(url = "https://github.com/tidymass/packages_repo/raw/main/packages/masscleaner_1.0.6.tar.gz",
-                       destfile = "masscleaner_1.0.6.tar.gz")
-}
+install_tidymass <-
+  function(packages = c("core", "all"),
+           from = c("gitee", "gitlab", "github")) {
+    packages <- match.arg(packages)
+    from <- match.arg(from)
+    temp_path <- tempdir()
+    dir.create(temp_path, showWarnings = FALSE, recursive = TRUE)
+    
+    file <-
+      readr::read_csv(
+        "https://raw.githubusercontent.com/tidymass/packages_repo/main/packages/file.csv",
+        show_col_types = FALSE
+      )
+    
+    if (packages == "core") {
+      package_list <-
+        c(
+          "masstools",
+          "massdataset",
+          "metid",
+          "massstat",
+          "massqc",
+          "massprocessor",
+          "masscleaner",
+          "metpath",
+          "tidymass"
+        )
+    } else{
+      package_list <-
+        c(
+          "masstools",
+          "massdataset",
+          "metid",
+          "massstat",
+          "massqc",
+          "massprocessor",
+          "masscleaner",
+          "metpath",
+          "tidymass",
+          "massconverter",
+          "massdatabase"
+        )
+    }
+    
+    package_list %>%
+      purrr::walk(function(x) {
+        message("Install ", x, "...")
+        ##install masstools first
+        url <-
+          paste0(
+            "https://github.com/tidymass/packages_repo/raw/main/packages/",
+            file$file_name.y[file$package == x]
+          )
+        utils::download.file(url = url,
+                             destfile = file.path(temp_path, file$file_name.y[file$package == x]))
+        
+        tryCatch(
+          detach(name = paste0("package:", x)),
+          error = function(e) {
+            message(x, " in not loaded")
+          }
+        )
+        
+        install.packages(file.path(temp_path, file$file_name.y[file$package == x]),
+                         repos = NULL,
+                         type = "source")
+        
+        unlink(file.path(temp_path, file$file_name.y[file$package == x]))
+      })
+    message("All done.")
+  }
